5b4735d67a10065a7f54bf0ecaa6caf1d21efe28
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-11 13:39:35 +0500

    feat: simple-button

---
+++ src/components/shared/ui/simple-button/simple-button.tsx
@@ -0,0 +1,21 @@
+import { memo, ButtonHTMLAttributes } from "react";
+import Styles from "./simple-button.module.css";
+
+interface Props {
+  children: string;
+  type: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  onClick: VoidFunction;
+  extraClass?: string;
+}
+
+const SimpleButton = memo(({ children, type, onClick, extraClass }: Props) => (
+  <button
+    className={`${Styles.button} ${extraClass}`}
+    type={type}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+));
+
+export default SimpleButton;
