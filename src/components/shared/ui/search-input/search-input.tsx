d757edb416d7d4dd28e0e2e4c0d56dd256c9da07
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-11 15:53:54 +0500

    refactor: вынести placeholder search-input

--- src/components/shared/ui/search-input/search-input.tsx
+++ src/components/shared/ui/search-input/search-input.tsx
@@ -3,15 +3,18 @@ import Styles from "./search-input.module.css";
 
 interface Props {
   children?: ReactNode;
+  placeholder?: string;
   onChange: VoidFunction;
   extraClass?: string;
 }
 
-const SearchInput = memo(({ children, onChange, extraClass }: Props) => (
-  <div className={`${Styles.cnt} ${extraClass}`} onChange={onChange}>
-    <input type="text" placeholder="Введите название" />
-    {children}
-  </div>
-));
+const SearchInput = memo(
+  ({ children, placeholder, onChange, extraClass }: Props) => (
+    <div className={`${Styles.cnt} ${extraClass}`} onChange={onChange}>
+      <input className={Styles.input} type="text" placeholder={placeholder} />
+      {children}
+    </div>
+  ),
+);
 
 export default SearchInput;
