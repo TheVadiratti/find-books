0d51dc950bc7d93d43d970aad7ce6c1498eef63a
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-11 19:20:21 +0500

    feat: book-grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import BooksGrid from "@/components/widgets/books-grid/books-grid";
 import Styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={Styles.main}>
-      <div />
+      <p>Found 446 results</p>
+      <BooksGrid />
     </main>
   );
 }
---
+++ src/components/widgets/books-grid/books-grid.tsx
@@ -0,0 +1,11 @@
+import { memo } from "react";
+// import BookCard from "@/components/entities/book-card/book-card";
+import Styles from "./books-grid.module.css";
+
+const BooksGrid = memo(() => (
+  <ul className={Styles.cnt}>
+    <div />
+  </ul>
+));
+
+export default BooksGrid;
