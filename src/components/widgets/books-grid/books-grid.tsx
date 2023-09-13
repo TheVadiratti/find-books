0d15fbd2e616c0f810aecf82e1387245ddf1795c
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-13 14:27:20 +0500

    feat: подключить данные к book-grid

--- src/components/widgets/books-grid/books-grid.tsx
+++ src/components/widgets/books-grid/books-grid.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { memo } from "react";
-// import BookCard from "@/components/entities/book-card/book-card";
+import { useAppSelector } from "@/store/types/hooks";
+import BookCard from "@/components/entities/book-card/book-card";
 import Styles from "./books-grid.module.css";
 
-const BooksGrid = memo(() => (
-  <ul className={Styles.cnt}>
-    <div />
-  </ul>
-));
+const BooksGrid = memo(() => {
+  const foundBooks = useAppSelector((state) => state.searchSlice.foundBooks);
+
+  return (
+    <ul className={Styles.cnt}>
+      {foundBooks.map((item) => (
+        <BookCard
+          image={item.volumeInfo.imageLinks?.smallThumbnail || ""}
+          category={item.volumeInfo.categories?.join("/") || ""}
+          name={item.volumeInfo.title}
+          author={item.volumeInfo.authors?.join(", ") || ""}
+          key={item.id}
+        />
+      ))}
+    </ul>
+  );
+});
 
 export default BooksGrid;
