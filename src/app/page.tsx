ae517d23aa48e11d10598df045dbe5d2399998b9
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-13 14:27:41 +0500

    feat: отобразить результат поиска

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import BooksGrid from "@/components/widgets/books-grid/books-grid";
+import { useAppSelector } from "@/store/types/hooks";
 import Styles from "./page.module.css";
 
 export default function Home() {
+  const total = useAppSelector((state) => state.searchSlice.foundTotal);
+  const isSearchSuccess = useAppSelector(
+    (state) => state.searchSlice.status.isSuccess,
+  );
+
   return (
     <main className={Styles.main}>
-      <p>Found 446 results</p>
+      {(total || isSearchSuccess) && <p>{`Found ${total} results`}</p>}
       <BooksGrid />
     </main>
   );
