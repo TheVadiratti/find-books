d0a1b2f9bb3661ec4f263660ea429ae945200000
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-13 14:24:54 +0500

    feat: подключить данные к book-card

--- src/components/entities/book-card/book-card.tsx
+++ src/components/entities/book-card/book-card.tsx
@@ -1,19 +1,22 @@
 import { memo } from "react";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import Styles from "./book-card.module.css";
 
 interface Props {
-  image: StaticImageData;
+  image: string;
+  category: string;
+  name: string;
+  author: string;
 }
 
-const BookCard = memo(({ image }: Props) => (
+const BookCard = memo(({ image, category, name, author }: Props) => (
   <li className={Styles.card}>
     <div className={Styles.imageCnt}>
       <Image src={image} alt="book preview" fill objectFit="contain" />
     </div>
-    <p className={Styles.category}>Computers</p>
-    <p className={Styles.name}>Node.js Путеводитель по технологии</p>
-    <p className={Styles.author}>Кирилл Сухов</p>
+    <p className={Styles.category}>{category}</p>
+    <p className={Styles.name}>{name}</p>
+    <p className={Styles.author}>{author}</p>
   </li>
 ));
 
