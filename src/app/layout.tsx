2526f34e9e69192b7556dcfa3f8a6449011b4c92
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-14 20:17:18 +0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,9 @@ import { Provider } from "react-redux";
 import store from "@/store/store";
 import "../styles/globals.css";
 import "../styles/vars.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Find books",
-  description: "Find books by Google Books API",
-};
 
 export default function RootLayout({
   children,
