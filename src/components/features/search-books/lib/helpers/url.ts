7cd5f05c8dc2b1f610e84c9a7d67bc8f4843c3b6
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-14 10:42:54 +0500

    refactor: исправить хардкод composeUrl

--- src/components/features/search-books/lib/helpers/url.ts
+++ src/components/features/search-books/lib/helpers/url.ts
@@ -1,13 +1,36 @@
-/* eslint-disable prettier/prettier */
 /* eslint-disable import/prefer-default-export */
 
 import { API_KEY } from "../constants/config";
 
 export const composeUrl = (
   baseUrl: string,
-  params: { search: string; category: string; sortingBy: string; startIndex?: number },
+  params: {
+    search: string;
+    category: string;
+    sortingBy: string;
+    startIndex?: number;
+  },
 ) => {
-  // TODO Исправить хардкод
   const { search, category, sortingBy, startIndex } = params;
-  return `${baseUrl}${search && `q=${search.toLowerCase()}`}${category !== "all" && `+subject:${category.toLowerCase()}`}${sortingBy && `&orderBy=${sortingBy.toLowerCase()}`}${startIndex ? `&startIndex=${String(startIndex)}&maxResults=30` : ""}&key=${API_KEY}`;
+  let url = baseUrl;
+
+  if (search) {
+    url += `q=${search.toLowerCase()}`;
+  }
+
+  if (category !== "all") {
+    url += `+subject:${category.toLowerCase()}`;
+  }
+
+  if (sortingBy) {
+    url += `&orderBy=${sortingBy.toLowerCase()}`;
+  }
+
+  if (startIndex) {
+    url += `&startIndex=${String(startIndex)}&maxResults=30`;
+  }
+
+  url += `&key=${API_KEY}`;
+
+  return url;
 };
