5565a8c3719d66e11765d85fe83c7c9660715640
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-14 10:28:22 +0500

    fix: ошибки загрузки данных

--- src/components/widgets/books-grid/books-grid.tsx
+++ src/components/widgets/books-grid/books-grid.tsx
@@ -7,15 +7,16 @@ import Styles from "./books-grid.module.css";
 
 const BooksGrid = memo(() => {
   const foundBooks = useAppSelector((state) => state.searchSlice.foundBooks);
+  // TODO переделать хардкод BookCard
 
   return (
     <ul className={Styles.cnt}>
       {foundBooks.map((item) => (
         <BookCard
-          image={item.volumeInfo.imageLinks?.smallThumbnail || ""}
-          category={item.volumeInfo.categories?.join("/") || ""}
-          name={item.volumeInfo.title}
-          author={item.volumeInfo.authors?.join(", ") || ""}
+          image={item.volumeInfo?.imageLinks?.smallThumbnail || ""}
+          category={item.volumeInfo?.categories?.join("/") || ""}
+          name={item.volumeInfo?.title}
+          author={item.volumeInfo?.authors?.join(", ") || ""}
           key={item.id}
         />
       ))}
